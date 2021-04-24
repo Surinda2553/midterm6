f1c5dd80e8a9b689da26569c67335b81b60450e0
Surinda Sarakum <surindasarakum18@gmail.com>
2021-04-24 15:18:04 +0700

    Add project files.

---
+++ midterm6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace midterm6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Char symbol = Console.ReadLine()[0];
+            int width = Convert.ToInt32(Console.ReadLine());
+            int height = Convert.ToInt32(Console.ReadLine());
+            int step = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= step; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    for (int k = 0; k < width * i; k++)
+                    {
+                        Console.Write(symbol);
+                    }
+                    Console.Write("\n");
+                }
+            };
+
+        }
+    }
+}
